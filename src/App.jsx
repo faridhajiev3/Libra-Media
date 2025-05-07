90bc29887cde88e7ca79b68510e0fd2ba414b571
Gulban <gulbanea-bp216@code.edu.az>
2025-05-07 15:55:10 +0400

    account

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PageContainer from "./container/PageContainer";
 import PageDetail from "./pages/Detail/PageDetail";
 import Cart from "./pages/Cart/Cart";
 import Maps from "./components/Maps";
+<<<<<<< HEAD:Libra Media/src/App.jsx
 import Wishllst from "./pages/Wishlist/Wishllst";
 import CreditCard from "./components/CreditCard";
 
@@ -26,6 +27,28 @@ function App() {
           <Route path="/credit" element={<CreditCard />} />
         </Routes>
       </PageContainer>
+=======
+import Login from "./pages/Login/login";
+import SignUp from "./pages/SignUp/SignUp";
+
+function App() {
+  return (
+    <PageContainer>
+      <Navbar />
+      <Routes>
+        <Route >
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<PageDetail />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/map" element={<Maps />} />
+        </Route>
+        <Route >
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp/>} />
+         </Route>
+      </Routes>
+>>>>>>> bdae3b3 (account):src/App.jsx
       <Footer />
     </>
   );
